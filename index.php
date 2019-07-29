1c79ed87dfebf6714cde3cf46534382b535ee84e
kuswandanu <kuswandanu@gmail.com>
2019-07-29 13:54:34 +0700

    connection

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = getenv("db-server");
+    $user = getenv("db-user");
+    $pass = getenv("db-pass");
+    $db = getenv("db-name");
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
